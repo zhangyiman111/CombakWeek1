2ac5c8b0deb224489d95e56733d8cf2e727c1629
zhangyiman <841589912@qq.com>
2019-12-02 13:40:59 +0800

    ddd

---
+++ app/src/main/java/com/bawei/combakweek1/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.combakweek1.base;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ *@auther:张奕漫
+ *@Date: 2019/12/2
+ *@Time:13:34
+ *@Description:
+ * */
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/bawei/combakweek1/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.combakweek1.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bawei.combakweek1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
